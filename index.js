c4ab2d0c8f7d3e2e7e30c25ef1a59ecf0b9568ed
Sankalp Sangle <sanku.sangle@gmail.com>
2022-04-07 21:16:34 -0400

    initial

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 //Starts the server
 
-let server = app.listen(4000, function () {
+let server = app.listen(process.env.port || 4000, function () {
   console.log("Server is running");
 });
 
--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:4000");
+let socket = io.connect("https://" + window.location.hostname);
 let divVideoChatLobby = document.getElementById("video-chat-lobby");
 let divVideoChat = document.getElementById("video-chat-room");
 let joinButton = document.getElementById("join");
